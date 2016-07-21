ed4b64574f9a3c168a75d6b7dae20fe78ffecd71
outyua <wanglv@hotcast.cn>
2016-07-21 19:20:01 +0800

    finish get state delete

--- test/config.inc.php
+++ test/config.inc.php
@@ -3,8 +3,8 @@
 require_once('../src/config.inc.php');
 require_once("../src/auth.php");
 
-$ak = '20ef843d139b5ed2557139cba110015a766861d4';
-$sk = '569e28b2698a34f708c0ee054ab0de6f2e965ef3';
+$ak = '';
+$sk = '';
 
 wcs_set_key($ak, $sk);
 
